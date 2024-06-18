16b77ce4b028cd1f25a1dcc6dab9318e9f39ef92
韋辰 劉 <q09855213@gmail.com>
2024-06-18 17:41:07 +0800

    專案前配置

---
+++ LoginSystemAndNews/DataAccess/MemberRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Data.SQLite;
+using System;
+
+namespace LoginSystemAndNews.DataAccess
+{
+    public class MemberRepository
+    {
+        
+    }
+}
